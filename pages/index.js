c031f2f5c688c88c1092957654929a0f4813bbca
Matt McMeans <mrmcmans@memphis.edu>
2024-02-08 20:32:30 -0600

    Add events page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
+function HomePage() {
   return (
-    <div className={styles.container}>
-      <h1>Hello Next World!</h1>
+    <div>
+      <h1>Home Page</h1>
     </div>
-  )
+  );
 }
+
+export default HomePage;
