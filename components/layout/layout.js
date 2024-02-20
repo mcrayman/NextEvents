9edfd0c79bd046e96494c10b31efe3748cf59040
Matt McMeans <mrmcmans@memphis.edu>
2024-02-19 22:30:11 -0600

    feat: Main header component

---
+++ components/layout/layout.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+function Layout(props) {
+  return (
+    <Fragment>
+      <main>
+        {props.children}
+      </main>
+    </Fragment>
+  );
+}
+
+export default Layout;
---
+++ components/layout/main-header.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+function MainHeader() {
+  return (
+    <header>
+      <div>
+        <Link href='/'>NextEvents</Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/events'>Browse All Events</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainHeader;
