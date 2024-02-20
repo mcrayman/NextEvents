6fc8b47a7b4c76ad32b5c3620e0720f8d44b8937
Matt McMeans <mrmcmans@memphis.edu>
2024-02-19 22:25:01 -0600

    feat: Dynamically load event detail pages

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,35 @@
-function EventDetail() {
-    return (
-        <div>
-        <h1>Event Detail</h1>
-        </div>
-    );
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
+function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.data}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
-export default EventDetail;
+export default EventDetailPage;
